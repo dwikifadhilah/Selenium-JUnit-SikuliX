f12d50fb17cd66d03dc1d4a0cbb896ca2dfee76e
dwikifadhilah <dwkfadhilah@gmail.com>
2022-09-01 10:25:00 +0700

    add image directory

--- src/test/java/com/testsikuli/BaseTest.java
+++ src/test/java/com/testsikuli/BaseTest.java
@@ -1,9 +1,11 @@
 package com.testsikuli;
 
+import com.testsikuli.pageobject.CalculatorPage;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.sikuli.script.ImagePath;
 import org.sikuli.script.Screen;
 
 public class BaseTest {
@@ -14,7 +16,8 @@ public class BaseTest {
     @Before
     public void openBrowser(){
         
-        System.setProperty("webdriver.chrome.driver", "D:\\Aplikasi\\Web Driver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver.exe");
+        ImagePath.add(System.getProperty("user.dir"));
         
         driver = new ChromeDriver();
         driver.manage().window().maximize();
